166f5858da4682c552f7c05d4545d3b655f5648a
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-03 21:30:40 +0900

    QRコード読み込みの雛形を実装

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,32 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import jsQR from "jsqr";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+(async () => {
+  const video = document.createElement("video");
+  const foundCameraCanvas = document.getElementById("camera-canvas");
+  const cameraCanvas = foundCameraCanvas instanceof HTMLCanvasElement
+    ? foundCameraCanvas
+    : document.createElement("canvas");
+  const canvas = cameraCanvas.getContext("2d", {willReadFrequently: true}) ?? new CanvasRenderingContext2D();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  video.srcObject = await navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}});
+  video.setAttribute("playsinline", "");
+  video.play();
+  requestAnimationFrame(tick);
+
+  function tick() {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+      cameraCanvas.height = video.videoHeight;
+      cameraCanvas.width = video.videoWidth;
+      canvas.drawImage(video, 0, 0, cameraCanvas.width, cameraCanvas.height);
+      const imageData = canvas.getImageData(0, 0, cameraCanvas.width, cameraCanvas.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: "dontInvert",
+      });
+      if (code) {
+        console.log(code.data);
+      }
+    }
+    requestAnimationFrame(tick);
+  }
+})();
