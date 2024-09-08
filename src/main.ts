7e5f442057bdccb441dacb8536098ad1ce6e8f4a
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-08 11:33:54 +0900

    QRコードで読みったものに枠線をつけるようにした

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 import jsQR from "jsqr";
+import {Point} from "jsqr/dist/locator";
 
 (async () => {
   const video = document.createElement("video");
@@ -14,6 +15,15 @@ import jsQR from "jsqr";
   video.play();
   requestAnimationFrame(tick);
 
+  function drawLine(begin: Point, end: Point, color: string | CanvasGradient | CanvasPattern) {
+    canvas.beginPath();
+    canvas.moveTo(begin.x, begin.y);
+    canvas.lineTo(end.x, end.y);
+    canvas.lineWidth = 4;
+    canvas.strokeStyle = color;
+    canvas.stroke();
+  }
+
   function tick() {
     if (video.readyState === video.HAVE_ENOUGH_DATA) {
       cameraCanvas.height = video.videoHeight;
@@ -24,6 +34,10 @@ import jsQR from "jsqr";
         inversionAttempts: "dontInvert",
       });
       if (code) {
+        drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+        drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+        drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+        drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
         console.log(code.data);
       }
     }
